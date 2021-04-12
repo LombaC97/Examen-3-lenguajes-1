050e1459ae544d6702dd2f050ae0b084ece9cafd
Carlos Gonzalez <carlosegl10@gmail.com>
2021-04-12 15:27:04 -0400

    Implementacion primera pregunta

---
+++ Pregunta 1/Pregunta1i.rb
@@ -0,0 +1,58 @@
+#En ruby no existen clases abstractas como tal, pero podemos simular una clase
+#que define estos métodos, sin embargo, no incluye comportamiento
+class Secuencia
+    def agregar
+    end
+    def remover
+    end
+    def vacio
+    end
+end
+#Pila hereda de Secuencia, y va a tener un array de elements que básicamente va a representar la pila
+class Pila < Secuencia
+    attr_accessor :elements
+    #Inicializamos el array de los elementos, y por cada elemento que tenga el array con el que se crea,
+    #se llama a la funcion de agregar para que los agregue en el orden similar a una pila
+    def initialize(elementos)
+        @elements = []
+        elementos.each {|element| self.agregar(element)}
+    end
+    #Agregamos elementos al final
+    def agregar(elemento)
+        @elements << elemento
+    end
+    #Removemos elementos del final
+    def remover
+        return @elements.pop
+    end
+    #Devuelve si el @elements esta vacio
+    def vacio
+        return @elements.empty?
+    end
+end
+#Cola hereda de Secuencia, y va a tener un array de elements que básicamente va a representar la cola
+class Cola < Secuencia
+    attr_accessor :elements
+    #Inicializamos el array de elementos, basicamente llamamos a la función de agregar por cada elemento 
+    #para que lo agregue similar a una cola
+    def initialize(elementos)
+        @elements = []
+        elementos.each {|element| self.agregar(element)}
+    end
+    #Agregamos los elementos al principio
+    def agregar(elemento)
+        @elements.insert(0,elemento)
+    end
+    #Removemos los elementos del final
+    def remover
+        return @elements.pop
+    end
+    #Retornamos si la cola esta vacia
+    def vacio
+        return @elements.empty?
+    end
+end
+
+
+
+
---
+++ Pregunta 1/Pregunta1ii.rb
@@ -0,0 +1,105 @@
+load 'Pregunta1i.rb'
+#El grafo lo representamos con un objeto del tipo grafo
+#Es importante tener en cuenta que estamos representando cada
+#Posicion del grafo como un Diccionario. Las claves son los nodos
+#Y los values son las listas de adyacencias que contienen
+#La informacion sobre a que nodo se conecta
+class Grafo
+    attr_accessor :grafo
+    def initialize
+        @grafo = Hash.new()
+        #Aqui creé un grafo de ejemplo
+        self.insert(0,[1,6,8])
+        self.insert(1,[0,4,6,9])
+        self.insert(2,[4,6])
+        self.insert(3,[4,5,8])
+        self.insert(4,[1,2,3,5,9])
+        self.insert(5,[3,4])
+        self.insert(6,[0,1,2])
+        self.insert(7,[8,9])
+        self.insert(8,[0,3,7])
+        self.insert(9,[1,4,7])
+        #Este nodo, el 10, no es alcanzable desde ningun lado
+        self.insert(10,[])
+    end
+    #En el diccionario de grafo, se colocan los pares clave/valor
+    def insert(clave, valor)
+        @grafo[clave] = valor
+    end
+end
+#Definimos una clase busqueda. Esta clase ya tiene casi todo el comportamiento definido
+#A excepcion del orden de busqueda que va a realizar, en todo caso, eso se define en DFS y BFS
+class Busqueda
+    #buscar setea las condiciones iniciales y devuelve realizar busqueda, que como tal es la que se encarga
+    #de iniciar la busqueda
+    def buscar(d,h)
+        visitados_inicial = Array.new() << d
+        if (d == h)
+            return "Nodos recorridos 0"
+        end
+        return self.realizar_busqueda(d,h, 1, visitados_inicial, [], nil)
+    end
+    #Realizar busqueda recibe el nodo inicial, el final, un contador que inicialmente es 0
+    #Maneja un array de visitados que inicialmente solo tiene el nodo de partida, y nodos que es
+    #un array donde se van guardando la cantidad de nodos recorridos cada vez que logra llegar al nodo
+    def realizar_busqueda(d, h, count, visitados, nodos, anterior)
+        #Tomamos el array de elementos de la variable global tipo Grafo (Decidi manejarla global por simplicidad)
+        array_elementos = $g.grafo[d]
+        #Basicamente pila_cola va a depender de si se trata de un DFS o de un BFS. Este comportamiento se da en la funcion
+        #Orden definida en cada uno
+        
+        pila_cola = self.orden(array_elementos)
+        #Si la pila_cola incluye el h, retornamos true
+        if pila_cola.elements.include? h 
+           
+            return true
+        #En cualquier otro caso
+        else
+            #Recorremos la pila_cola mientras que no este vacias
+            while not(pila_cola.vacio)
+                #Tomamos un elemento
+                elem = pila_cola.remover
+                #Si el elemento todavia no ha sido visitado (no esta en el array de visitados)
+                if not(visitados.include? elem)
+                    #Lo ponemos en el array
+                    visitados << elem
+                    #Y si llega a dar true la llamada recursiva (es decir, encontro el elemento)
+                    if realizar_busqueda(elem, h , count + 1 , visitados, nodos, d) == true
+                        #Entonces agregamos el count + 1 al array de nodos
+                        nodos << count + 1
+                    end 
+                end
+            end
+        end
+        #Si el array de nodos nunca tuvo nada, esta vacio, entonces no era alcanzable. Retornamos -1
+        if nodos.empty?
+            return -1
+        end
+        #En otro caso, retornamos la cantidad de nodos recorridos
+        #Es importante aclarar que solo retorna los recorridos para la primera vez que encontró el elemento
+        #Si se quiere que retorne todos los caminos en los que lo encontró es tan sencillo como retornar
+        #nodos en lugar de nodos[0]
+        return "Nodos recorridos #{nodos[0]}"
+    end
+end
+#DFS basicamente crea una pila con los elementos y la retorna
+class DFS < Busqueda
+    def orden(elementos)
+        return Pila.new(elementos)
+    end
+end
+#BFS basicamente crea una cola con los elementos y la retorna
+class BFS < Busqueda
+    def orden(elementos)
+        return Cola.new(elementos)
+    end
+end
+
+$g = Grafo.new
+
+dfs = DFS.new()
+bfs = BFS.new()
+
+puts dfs.buscar(3,6)
+
+puts bfs.buscar(3,6)
